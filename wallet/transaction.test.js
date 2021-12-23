b255b06e12d10b4190049dfb059f18a85724b949
Krisda Siangchaew <krisda.siangchaew@gmail.com>
2021-12-24 01:59:47 +0700

    Transaction update

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -30,23 +30,40 @@ describe('Transaction', () => {
         expect(Transaction.verifyTransaction(transaction)).toBe(true)
     })
 
-    it('invalidates an invalid transaction', () => {
+    it('invalidates a corrupt transaction', () => {
         transaction.outputs[0].amount = 50000
         expect(Transaction.verifyTransaction(transaction)).toBe(false)
     })
+
+    describe('transacting with an amount that exceeds the balance', () => {
+        beforeEach(() => {
+            amount = 50000
+            transaction = Transaction.newTransaction(wallet, recipient, amount)
+        })
+    
+        it('does not create transaction', () => {
+            expect(transaction).toEqual(undefined)
+        })
+    })
+
+    describe(`and updating transaction`, () => {
+        let nextAmount, nextRecipient
+    
+        beforeEach(() => {
+            nextAmount = 20
+            nextRecipient = 'n3xt-4ddr355'
+            transaction = transaction.update(wallet, nextRecipient, nextAmount)
+        })
+    
+        it('subtracts the next amount from the sender output', () => {
+            expect(transaction.outputs.find(output => output.address === wallet.publicKey).amount)
+            .toEqual(wallet.balance - amount - nextAmount)
+        })
+
+        it('outputs the amount for the next recipient', () => {
+            expect(transaction.outputs.find(output => output.address === nextRecipient).amount)
+            .toEqual(nextAmount)
+        })
+    })
 })
 
-// describe('transacting with an amount that exceeds the balance', () => {
-//     let transaction, wallet, recipient, amount
-
-//     beforeEach(() => {
-//         wallet = new Wallet()
-//         amount = 50000
-//         recipient = 'r3c1p13nt'
-//         transaction = Transaction.newTransaction(wallet, recipient, amount)
-//     })
-
-//     it('does not create transaction', () => {
-//         expect(transaction).toEqual(undefined)
-//     })
-// })
